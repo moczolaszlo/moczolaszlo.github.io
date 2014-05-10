d0170a6ea40b19d2f5951fbfc0eabb431439027c
moczolaszlo <moczo.laszlo@gmail.com>
2014-05-11 00:28:45 +0200

    template, json, hu
    - template
    - json
    - en + hu

--- CV/cv.js
+++ CV/cv.js
@@ -1,19 +1,47 @@
-$(document).on('ready', function(){
+var CV = {
 
-	$('footer a').on('click', function(e){
-		var action = this.href.split('#')[1];
-		if (action) {
-			e.preventDefault();
-			switch (action) {
-				case 'print':
-					window.print();
-					break;
+	templates: {
+		contact : $.templates('#tpl-contact'),
+		skills : $.templates('#tpl-skills'),
+		experience : $.templates('#tpl-experience'),
+		footer : $.templates('#tpl-footer')
+	},
+	bindEvents: function(){
+		$('footer').on('click', 'a', function(e){
+			var action = this.href.split('#')[1];
+			if (action) {
+				e.preventDefault();
+				switch (action) {
+					case 'en':
+					case 'hu':
+						CV.generateSite(action);
+						break;
 
-				case 'top':
-					$('html, body').animate({scrollTop:0}, 'slow');
-					break;
+					case 'print':
+						window.print();
+						break;
+
+					case 'top':
+						$('html, body').animate({scrollTop:0}, 'slow');
+						break;
+				}
 			}
-		}
-	});
+		});
+	},
+	generateSite: function(lang) {
+		$.getJSON(lang + '.json', function(data){
+			console.log(data)
+			$('address').html(CV.templates.contact.render(data.contact));
+			$('#skills').html(CV.templates.skills.render(data.skills));
+			$('#experience').html(CV.templates.experience.render(data.experience));
+			$('footer').html(CV.templates.footer.render(data.footer));
+			$('html, body').animate({scrollTop:0});
+		});
+	},
+	init: function() {
+		CV.bindEvents();
+		CV.generateSite('hu');
+	}
+}
 
-});
+$(document).on('ready', CV.init);
